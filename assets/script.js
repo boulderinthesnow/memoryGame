036471e95cd78a645f36d73a70700b35fe3b2e09
Matthew Williams <mwilliamsnov6@gmail.com>
2015-05-15 15:38:46 -0700

    inital squares working

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+for (i = 0 ; i < 16 ; i++) {
+	var square = document.createElement('div')
+	square.style.width = "24%"
+	square.style.float = "left"
+	square.style.paddingBottom = "24%"
+	square.style.backgroundColor = "blue"
+	square.style.border = "1px solid indianRed"
+	document.body.appendChild(square)
+}
+
+
+
+	
